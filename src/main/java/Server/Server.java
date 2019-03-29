baf34eb8c9d3ba649f805e5fedd27abd0e7cb471
GODOmegaPi <guillaume.laubscher@heig-vd.ch>
2019-03-29 11:18:17 +0100

    Change server to a multithreaded one and added some tests

--- src/main/java/Server/Server.java
+++ src/main/java/Server/Server.java
@@ -1,81 +1,17 @@
 package Server;
 
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import javax.script.ScriptEngine;
-import javax.script.ScriptEngineManager;
-import javax.script.ScriptException;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.net.ServerSocket;
-import java.net.Socket;
-
 public class Server {
-    private ServerSocket serverSocket;
-    private Socket clientSocket;
-    private PrintWriter out;
-    private BufferedReader in;
-
-    public void start(int port) {
-        System.out.println("Server is booting");
-
-        try {
-            serverSocket = new ServerSocket(port);
-            clientSocket = serverSocket.accept();
-            out = new PrintWriter(clientSocket.getOutputStream(), true);
-            in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-
-            String json = in.readLine();
-            JSONObject obj;
-
-            try {
-                obj = new JSONObject(json);
-                String calcul = obj.getString("Calculation");
-
-                // auto eval of a string to a math function using javascript
-                ScriptEngineManager manager = new ScriptEngineManager();
-                ScriptEngine engine = manager.getEngineByName("js");
-                Object result = engine.eval(calcul);
-
-                obj.put("Error", "");
-                obj.put("Result", result.toString());
-            } catch (JSONException e){
-                obj = new JSONObject();
-                obj.put("Calculation", json);
-                obj.put("Error", e.toString());
-                obj.put("Result", "");
-            } catch (ScriptException e){
-                obj = new JSONObject();
-                obj.put("Calculation", json);
-                obj.put("Error", e.toString());
-                obj.put("Result", "");
-            }
-
-            out.println(obj.toString());
-
-            stop();
-
-        } catch (Exception e){
-            e.printStackTrace();
-        }
-    }
-
-    public void stop() {
-        System.out.println("Server is shutting down");
-
-        try {
-            in.close();
-            out.close();
-            clientSocket.close();
-            serverSocket.close();
-        } catch (Exception e){
-            e.printStackTrace();
-        }
-    }
     public static void main(String[] args) {
-        Server server = new Server();
-        server.start(6666);
+        MultiThreadedServer server = new MultiThreadedServer(6666);
+        new Thread(server).start();
+
+        try {
+            Thread.sleep(500 * 1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Stopping Server");
+        server.stop();
     }
 }
--- src/test/java/ClientTest.java
+++ src/test/java/ClientTest.java
@@ -5,10 +5,63 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ClientTest {
     @Test
-    public void givenClient_whenServerRespondsWhenStarted_thenCorrect() {
+    public void oneClientShouldReceiveTheCorrectJSONWhenAskingTheServer() {
         Client client = new Client();
         client.startConnection("127.0.0.1", 6666);
         String response = client.sendMessage("{\"Calculation\": \"Math.pow(2,4) % 7\" }");
         assertEquals("{\"Error\":\"\",\"Calculation\":\"Math.pow(2,4) % 7\",\"Result\":\"2.0\"}", response);
+    }
+
+    @Test
+    public void fiveClientsShouldReceivesTheSameCorrectJSONWhenAskingTheServer() {
+        Client client1 = new Client();
+        Client client2 = new Client();
+        Client client3 = new Client();
+        Client client4 = new Client();
+        Client client5 = new Client();
+
+        client1.startConnection("127.0.0.1", 6666);
+        client2.startConnection("127.0.0.1", 6666);
+        client3.startConnection("127.0.0.1", 6666);
+        client4.startConnection("127.0.0.1", 6666);
+        client5.startConnection("127.0.0.1", 6666);
+
+        String response;
+
+        response = client1.sendMessage("{\"Calculation\": \"Math.pow(2,4) % 7\" }");
+        assertEquals("{\"Error\":\"\",\"Calculation\":\"Math.pow(2,4) % 7\",\"Result\":\"2.0\"}", response);
+        response = null;
+
+        response = client2.sendMessage("{\"Calculation\": \"Math.pow(2,4) % 7\" }");
+        assertEquals("{\"Error\":\"\",\"Calculation\":\"Math.pow(2,4) % 7\",\"Result\":\"2.0\"}", response);
+        response = null;
+
+        response = client3.sendMessage("{\"Calculation\": \"Math.pow(2,4) % 7\" }");
+        assertEquals("{\"Error\":\"\",\"Calculation\":\"Math.pow(2,4) % 7\",\"Result\":\"2.0\"}", response);
+        response = null;
+
+        response = client4.sendMessage("{\"Calculation\": \"Math.pow(2,4) % 7\" }");
+        assertEquals("{\"Error\":\"\",\"Calculation\":\"Math.pow(2,4) % 7\",\"Result\":\"2.0\"}", response);
+        response = null;
+
+        response = client5.sendMessage("{\"Calculation\": \"Math.pow(2,4) % 7\" }");
+        assertEquals("{\"Error\":\"\",\"Calculation\":\"Math.pow(2,4) % 7\",\"Result\":\"2.0\"}", response);
+        response = null;
+    }
+
+    @Test
+    public void oneClientShouldReceiveAnErrorMessageIfTheJSONSentIsWrong() {
+        Client client = new Client();
+        client.startConnection("127.0.0.1", 6666);
+        String response = client.sendMessage("{\"Calculation\": \"Math.pow(2,4) % 7\" ");
+        assertEquals("{\"Error\":\"The JSON is not well formated or is missing some informations!\",\"JSON\":\"{\\\"Calculation\\\": \\\"Math.pow(2,4) % 7\\\" \",\"Result\":\"\"}", response);
+    }
+
+    @Test
+    public void oneClientShouldReceiveAnErrorMessageIfTheCalculationSentIsWrong() {
+        Client client = new Client();
+        client.startConnection("127.0.0.1", 6666);
+        String response = client.sendMessage("{\"Calculation\": \"Math.pow(2 4) % 7\" }");
+        assertEquals("{\"Error\":\"The JSON is not well formated or is missing some informations!\",\"JSON\":\"{\\\"Calculation\\\": \\\"Math.pow(2 4) % 7\\\" }\",\"Result\":\"\"}", response);
     }
 }
