0d403edb527eaa19b4e80cebdeadf95debb7268b
GODOmegaPi <guillaume.laubscher@heig-vd.ch>
2019-03-29 11:20:08 +0100

    Changed the name of the class test from ClientTest to ServerTest

--- src/test/java/ServerTest.java
+++ src/test/java/ServerTest.java
@@ -3,7 +3,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class ClientTest {
+public class ServerTest {
     @Test
     public void oneClientShouldReceiveTheCorrectJSONWhenAskingTheServer() {
         Client client = new Client();
